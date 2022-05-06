cc316bc8d99cad69e04a5e1f43cb193f42bbd178
Andrew Zhu <me@andrew-zhu.com>
2022-05-05 22:48:29 -0700

    it's alive!

---
+++ tests/test.ts
@@ -0,0 +1,43 @@
+[
+    {
+        precedence: 0.5,
+        target: `attributes.${statName}.modifier`,
+        operator: StatementOperators.SET,
+        operand: {
+            operator: ExpressionOperators.MAX,
+            operands: [
+                {
+                    operator: ExpressionOperators.FLOOR,
+                    operands: [
+                        {
+                            operator: ExpressionOperators.MULTIPLY,
+                            operands: [
+                                {
+                                    operator: ExpressionOperators.ADD,
+                                    operands: [
+                                        {
+                                            operator: ExpressionOperators.DYNAMIC_VALUE,
+                                            operands: `attributes.${statName}.value`
+                                        },
+                                        {
+                                            operator: ExpressionOperators.STATIC_VALUE,
+                                            operands: -10
+                                        }
+                                    ]
+                                },
+                                {
+                                    operator: ExpressionOperators.STATIC_VALUE,
+                                    operands: 0.5
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    operator: ExpressionOperators.STATIC_VALUE,
+                    operands: -5
+                }
+            ]
+        }
+    }
+]
